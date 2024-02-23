5f6652021ddcafa92169136321ca1c06dc0ba66a
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-02-23 16:15:42 +0000

    sm6225-common: Update AB/VAB/kernel/fstab definitions to bengal_515
    [1] - Based on my topaz device (bengal_515) device tree
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -27,7 +27,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/android_t_baseline.mk)
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := gz
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
