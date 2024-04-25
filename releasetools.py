fe57b6ae3a79bc1276d0334a86ca81a487378317
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-04-25 17:34:41 +0000

    sm6225-common: releasetools: Flash vendor_boot image
    Change-Id: I3d30b71e97f6a106454760cab3105a10dc0d4f8c
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -26,4 +26,5 @@ def OTA_InstallEnd(info):
     AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
     AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
     AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
+    AddImage(info, "vendor_boot.img", "/dev/block/bootdevice/by-name/vendor_boot")
     return
